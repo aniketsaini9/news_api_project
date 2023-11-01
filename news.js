5c4aee1dc4b4135e84aa301d79133b287ba7a897
aniketsaini9 <141916292+aniketsaini9@users.noreply.github.com>
2023-11-02 01:04:55 +0530

    Update news.js
    updated expired API key

--- news.js
+++ news.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "c0722781ba6748eabef200d69a091285";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
